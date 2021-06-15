fe85b5018a6478d77c107f415099773a1f85a4e9
CaganAgabey <57146234+CaganAgabey@users.noreply.github.com>
2021-06-15 04:52:10 +0300

    v1.0.5

--- commands/help.js
+++ commands/help.js
@@ -6,24 +6,23 @@ module.exports.run = async (client, message, args) => {
   function colorToSignedBit(s) {
     return (parseInt(s.substr(1), 16) << 8) / 256;
 }
-
-let dil = db.fetch(`dil_${message.guildID}`) || "english";
-
-if (dil == "english") {
-  let prefix = db.fetch(`prefix_${message.guildID}`) || "."
-  let helpcommands = client.commands.filter(prop => prop.help.category == "help" && prop.help.name != "help")
+  
+  const dil = db.fetch(`dil_${message.guildID}`) || "english";
+  
+  if (dil == "english") {
+  const prefix = db.fetch(`prefix_${message.guildID}`) || ".";
+  const helpcommands = client.commands.filter(prop => prop.help.category == "help" && prop.help.name != "help");
   if (helpcommands.length == 0) return message.channel.createMessage(`There's not any commands in this category.`)
-  let helpcommandsmap = helpcommands.map(p => '<:rightarrow:709539888411836526> **' + prefix + p.help.name + '** ' + p.help.descriptionen + `\n`).join('')
-  if (!db.has(`botcekilis`)) message.channel.createMessage({embed: {title: '__**Commands**__', description: helpcommandsmap, color: colorToSignedBit("#2F3136"), footer: {text: client.user.username, icon_url: client.user.avatarURL || client.user.defaultAvatarURL}}})
-  if (db.has(`botcekilis`)) message.channel.createMessage({embed: {title: '__**Commands**__', description: helpcommandsmap + `\n \n<:rightarrow:709539888411836526> There is an active giveaway in this bot!\n**Giveaway** ${db.fetch(`botcekilis.english`)}\n**Join with** ${prefix}giveaway`, color: colorToSignedBit("#2F3136"), footer: {text: client.user.username, icon_url: client.user.avatarURL || client.user.defaultAvatarURL}}})
+  const helpcommandsmap = helpcommands.map(p => '<:rightarrow:709539888411836526> **' + prefix + p.help.name + '** ' + p.help.descriptionen + `\n`).join('');
+  message.channel.createMessage({embed: {title: '__**Commands**__', description: helpcommandsmap, color: colorToSignedBit("#2F3136"), footer: {text: client.user.username, icon_url: client.user.avatarURL || client.user.defaultAvatarURL}}})
 }
 
 if (dil == "turkish") {
-  let prefix = db.fetch(`prefix_${message.guildID}`) || "."
-  let helpcommands = client.commands.filter(prop => prop.help.category == "help" && prop.help.name != "help")
+  const prefix = db.fetch(`prefix_${message.guildID}`) || ".";
+  const helpcommands = client.commands.filter(prop => prop.help.category == "help" && prop.help.name != "help");
   if (helpcommands.length == 0) return message.channel.createMessage(`Bu kategoride herhangi bir komut yok.`)
-  let helpcommandsmap = helpcommands.map(p => '<:rightarrow:709539888411836526> **' + prefix + p.help.nametr + '** ' + p.help.descriptiontr + `\n`).join('')
-    if (!db.has(`botcekilis`)) message.channel.createMessage({
+  const helpcommandsmap = helpcommands.map(p => '<:rightarrow:709539888411836526> **' + prefix + p.help.nametr + '** ' + p.help.descriptiontr + `\n`).join('');
+  if (!db.has(`botcekilis`)) message.channel.createMessage({
       embed: {
         title: '__**Komutlar**__',
         description: helpcommandsmap,
@@ -34,7 +33,7 @@ if (dil == "turkish") {
         }
       }
     })
-    if (db.has(`botcekilis`)) message.channel.createMessage({embed: {title: '__**Komutlar**__', description: helpcommandsmap + `\n \n<:rightarrow:709539888411836526> Botta aktif bir çekiliş var!\n**Çekiliş** ${db.fetch(`botcekilis.turkish`)}\n**Katılmak için** ${prefix}çekiliş`, color: colorToSignedBit("#2F3136"), footer: {text: client.user.username, icon_url: client.user.avatarURL || client.user.defaultAvatarURL}}})
+    message.channel.createMessage({embed: {title: '__**Komutlar**__', description: helpcommandsmap, color: colorToSignedBit("#2F3136"), footer: {text: client.user.username, icon_url: client.user.avatarURL || client.user.defaultAvatarURL}}})
   }
 }
 
--- commands/stats.js
+++ commands/stats.js
@@ -2,33 +2,44 @@ const Eris = require("eris");
 const arkdb = require('ark.db');
 
 module.exports.run = async (client, message, args) => {
-  const db = client.db
-  function colorToSignedBit(s) {
-    return (parseInt(s.substr(1), 16) << 8) / 256;
-}
-
-let dil = db.fetch(`dil_${message.guildID}`) || "english";
-
-if (dil == "english") {
-  let prefix = db.fetch(`prefix_${message.guildID}`) || "."
-  if (db.has(`botcekilis`)) message.channel.createMessage({embed: {title: `**__Bot stats__**`, description: `**Users** ${Math.round(client.users.size * 1.5)}\n**Guilds** ${Math.round(client.guilds.size)}\n**If you want to support Suggestions** [Add bot](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Vote Suggestions bot](https://top.gg/bot/709351286922936362/vote)` + `\n \n<:rightarrow:709539888411836526> There is an active giveaway in the bot!\n**Giveaway** ${db.fetch(`botcekilis.english`)}\n**Join with** ${prefix}giveaway`, color: colorToSignedBit("#2F3136")}})
-  if (!db.has(`botcekilis`)) message.channel.createMessage({embed: {title: `**__Bot stats__**`, description: `**Users** ${Math.round(client.users.size * 1.5)}\n**Guilds** ${Math.round(client.guilds.size)}\n**If you want to support Suggestions** [Add bot](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Vote Suggestions bot](https://top.gg/bot/709351286922936362/vote)`, color: colorToSignedBit("#2F3136")}})
-}
-
-if (dil == "turkish") {
-  let prefix = db.fetch(`prefix_${message.guildID}`) || "."
-  if (db.has(`botcekilis`)) message.channel.createMessage({embed: {title: `**__Bot istatistikleri__**`, description: `**Kullanıcılar** ${Math.round(client.users.size * 1.5)}\n**Sunucular** ${Math.round(client.guilds.size)}\n**Suggestions'u desteklemek isterseniz** [Botu ekle](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Suggestions botu oyla](https://top.gg/bot/709351286922936362/vote)` + `\n \n<:rightarrow:709539888411836526> Botta aktif bir çekiliş var!\n**Çekiliş** ${db.fetch(`botcekilis.turkish`)}\n**Katılmak için** ${prefix}çekiliş`, color: colorToSignedBit("#2F3136")}})
-  if (!db.has(`botcekilis`)) message.channel.createMessage({embed: {title: `**__Bot istatistikleri__**`, description: `**Kullanıcılar** ${Math.round(client.users.size * 1.5)}\n**Sunucular** ${Math.round(client.guilds.size)}\n**Suggestions'u desteklemek isterseniz** [Botu ekle](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Suggestions botu oyla](https://top.gg/bot/709351286922936362/vote)`, color: colorToSignedBit("#2F3136")}})
-}
+	const db = client.db
+	
+	function colorToSignedBit(s) {
+		return (parseInt(s.substr(1), 16) << 8) / 256;
+	}
+	
+	const dil = db.fetch(`dil_${message.guildID}`) || "english";
+	
+	if (dil == "english") {
+		const prefix = db.fetch(`prefix_${message.guildID}`) || ".";
+		message.channel.createMessage({
+			embed: {
+				title: `**__Bot stats__**`,
+				description: `**Users (may not show correct number)** ${client.users.size}\n**Guilds** ${client.guilds.size}\n**If you want to support Suggestions** [Add bot](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Vote Suggestions bot](https://top.gg/bot/709351286922936362/vote)`,
+				color: colorToSignedBit("#2F3136")
+			}
+		})
+	}
+	
+	if (dil == "turkish") {
+		const prefix = db.fetch(`prefix_${message.guildID}`) || ".";
+		message.channel.createMessage({
+			embed: {
+				title: `**__Bot istatistikleri__**`,
+				description: `**Kullanıcılar (doğru sayıyı göstermeyebilir)** ${client.users.size}\n**Sunucular** ${client.guilds.size}\n**Suggestions'u desteklemek isterseniz** [Botu ekle](https://discord.com/api/oauth2/authorize?client_id=709351286922936362&permissions=8&scope=bot) **|** [Suggestions botu oyla](https://top.gg/bot/709351286922936362/vote)`,
+				color: colorToSignedBit("#2F3136")
+			}
+		})
+	}
 }
 
 module.exports.help = {
-  name: "stats",
-  nametr: "istatistikler",
-  aliase: ["istatistik", "stat", "istatistikler"],
-  descriptionen: "Shows the bot's stats.",
-  descriptiontr: "Botun istatistiklerini gösterir.",
-  usageen: "staff",
-  usagetr: "yetkili",
-  category: 'public'
+	name: "stats",
+	nametr: "istatistikler",
+	aliase: [ "istatistik", "stat", "istatistikler" ],
+	descriptionen: "Shows the bot's stats.",
+	descriptiontr: "Botun istatistiklerini gösterir.",
+	usageen: "staff",
+	usagetr: "yetkili",
+	category: 'public'
 }
